f453e13530fc9c1a270ff4c673ae021d346b8cc7
learnermrit <97528624+learnermrit@users.noreply.github.com>
2023-02-28 00:14:05 +0530

    Create Second Largest inarray.cpp

---
+++ Array/Easy/Second Largest inarray.cpp
@@ -0,0 +1,24 @@
+	int print2largest(int arr[], int n) {
+	    // code here
+	    int max1=INT_MIN;
+	    int max2=INT_MIN+1;
+	    for(int i=0;i<n;i++){
+	        if(arr[i]>max2){
+	            if(arr[i]>max1){
+	              max2=max1;
+	              max1=arr[i];
+	            }
+	            else if (arr[i]!=max1){
+	                max2=arr[i];
+	            }
+	            
+	            
+	        }
+	    }
+	    
+	    if(max2==INT_MIN+1||max2==INT_MIN){
+	        return -1;
+	    }
+	    else
+	    return max2;
+	}
